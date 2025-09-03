6c7043ab57807dda472c449a986dcd592aad6de4
mohammadmansour200 <abdultawab940@gmail.com>
2025-09-03 08:23:16 +0300

    Initial commit

---
+++ app/src/main/java/com/tahbeer/app/ui/theme/Type.kt
@@ -0,0 +1,41 @@
+package com.tahbeer.app.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.tahbeer.app.R
+
+val IBMPlexSansArabic = FontFamily(
+    Font(
+        resId = R.font.ibmplexsansarabic_regular,
+        weight = FontWeight.Normal,
+    ),
+    Font(
+        resId = R.font.ibmplexsansarabic_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.ibmplexsansarabic_semibold,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.ibmplexsansarabic_bold,
+        weight = FontWeight.Bold,
+    ),
+)
+
+val baseline = Typography()
+
+val Typography = Typography(
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = IBMPlexSansArabic),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = IBMPlexSansArabic),
+    bodySmall = baseline.bodySmall.copy(fontFamily = IBMPlexSansArabic),
+    labelLarge = baseline.labelLarge.copy(fontFamily = IBMPlexSansArabic),
+    labelMedium = baseline.labelMedium.copy(fontFamily = IBMPlexSansArabic),
+    labelSmall = baseline.labelSmall.copy(fontFamily = IBMPlexSansArabic),
+    titleLarge = baseline.titleLarge.copy(fontFamily = IBMPlexSansArabic),
+    titleMedium = baseline.titleMedium.copy(fontFamily = IBMPlexSansArabic),
+    titleSmall = baseline.titleSmall.copy(fontFamily = IBMPlexSansArabic),
+)
+
