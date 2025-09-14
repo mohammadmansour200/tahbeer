c80fb4d2126b5f719414bcdd1425f93e131c8649
mohammadmansour200 <abdultawab940@gmail.com>
2025-09-14 19:44:14 +0300

    refactor: Moved TranscriptionItem class to the wrong place

--- app/src/main/java/com/tahbeer/app/core/domain/model/MediaType.kt
+++ app/src/main/java/com/tahbeer/app/core/domain/model/MediaType.kt
@@ -1,39 +1,5 @@
 package com.tahbeer.app.core.domain.model
 
-import kotlinx.serialization.Serializable
-
 enum class MediaType {
     AUDIO, VIDEO, SUBTITLE
 }
-
-enum class TranscriptionStatus {
-    PROCESSING, ERROR_PROCESSING, SUCCESS
-}
-
-
-fun com.whispercpp.whisper.SubtitleEntry.toDomainModel(): SubtitleEntry {
-    return SubtitleEntry(
-        text = this.text,
-        startTime = this.startTime,
-        endTime = this.endTime
-    )
-}
-
-@Serializable
-data class SubtitleEntry(
-    val startTime: Long,
-    val endTime: Long,
-    val text: String
-)
-
-@Serializable
-data class TranscriptionItem(
-    val id: String,
-    val mediaUri: String,
-    val mediaType: MediaType,
-    val lang: String,
-    val title: String,
-    val status: TranscriptionStatus = TranscriptionStatus.PROCESSING,
-    val progress: Float? = null,
-    val result: List<SubtitleEntry>? = null
-)
---
+++ app/src/main/java/com/tahbeer/app/core/domain/model/TranscriptionItem.kt
@@ -0,0 +1,35 @@
+package com.tahbeer.app.core.domain.model
+
+import kotlinx.serialization.Serializable
+
+enum class TranscriptionStatus {
+    PROCESSING, ERROR_PROCESSING, SUCCESS
+}
+
+
+fun com.whispercpp.whisper.SubtitleEntry.toDomainModel(): SubtitleEntry {
+    return SubtitleEntry(
+        text = this.text,
+        startTime = this.startTime * 10,
+        endTime = this.endTime * 10
+    )
+}
+
+@Serializable
+data class SubtitleEntry(
+    val startTime: Long,
+    val endTime: Long,
+    val text: String
+)
+
+@Serializable
+data class TranscriptionItem(
+    val id: String,
+    val mediaUri: String,
+    val mediaType: MediaType,
+    val lang: String,
+    val title: String,
+    val status: TranscriptionStatus = TranscriptionStatus.PROCESSING,
+    val progress: Float? = null,
+    val result: List<SubtitleEntry>? = null
+)
