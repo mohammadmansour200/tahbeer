d8945b0db44922873d169ef4b3768a1bbffe8241
mohammadmansour200 <abdultawab940@gmail.com>
2025-10-16 17:13:13 +0300

    feat: Improved stylings for ExportBottomSheet.kt chips

--- app/src/main/java/com/tahbeer/app/details/presentation/components/ExportBottomSheet.kt
+++ app/src/main/java/com/tahbeer/app/details/presentation/components/ExportBottomSheet.kt
@@ -1,19 +1,20 @@
 package com.tahbeer.app.details.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -24,29 +25,42 @@ import com.tahbeer.app.details.presentation.DetailScreenAction
 
 @Composable
 fun ExportBottomSheet(
-    snackbarHostState: SnackbarHostState,
     onAction: (DetailScreenAction) -> Unit,
     transcriptionItem: TranscriptionItem
 ) {
     var selectedExportFormat by remember { mutableStateOf(ExportFormat.TXT) }
     val options = ExportFormat.entries.toTypedArray()
-
-    FlowRow(horizontalArrangement = Arrangement.SpaceEvenly) {
-        options.forEach {
-            EnhancedChip(
-                onClick = {
-                    selectedExportFormat = it
-                },
-                selected = selectedExportFormat == it,
-                label = {
-                    Text(text = it.name.lowercase())
-                },
-                selectedColor = MaterialTheme.colorScheme.tertiary,
-                contentPadding = PaddingValues(
-                    horizontal = 16.dp,
-                    vertical = 6.dp
+    Column {
+        FlowRow(
+            verticalArrangement = Arrangement.spacedBy(
+                space = 8.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalArrangement = Arrangement.spacedBy(
+                space = 8.dp,
+                alignment = Alignment.CenterHorizontally
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(horizontal = 8.dp, vertical = 12.dp)
+        ) {
+            options.forEach {
+                EnhancedChip(
+                    onClick = {
+                        selectedExportFormat = it
+                    },
+                    selected = selectedExportFormat == it,
+                    label = {
+                        Text(text = it.name.lowercase())
+                    },
+                    selectedColor = MaterialTheme.colorScheme.tertiary,
+                    contentPadding = PaddingValues(
+                        horizontal = 16.dp,
+                        vertical = 6.dp
+                    )
                 )
-            )
+            }
         }
         Button(
             modifier = Modifier
